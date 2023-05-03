58fea877b7873374a30e857cb5f99c15c9618134
vivvieek <spvivek.babu@gmail.com>
2023-05-03 12:26:19 +0530

    project copy upload

---
+++ styles/popup.js
@@ -0,0 +1,15 @@
+function popup(){
+    alert('Not assigned')
+}
+
+function search(){
+    alert('Can`t search')
+}
+
+function districts(){
+    alert('Only TVM, KLM, PTA, ALP and KTM available !')
+}
+
+function login(){
+    alert('Can`t login or signup')
+}
